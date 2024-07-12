b5f53b6d047edd3759fd6de91e2991b945747ae2
Mohamed Ibrahim <57298210+mo-ibrahem@users.noreply.github.com>
2024-07-12 02:20:18 +0100

    finished the user authentication

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -13,11 +13,11 @@ const Navigation = ()=> {
                 <Link className="nav-link" to='/shop'>
                     SHOP
                 </Link>
-                <Link className="nav-link" to='/sign-in'>
+                <Link className="nav-link" to='/auth'>
                     SIGN IN
                 </Link>
+             </div>
             </div>
-        </div>
           <Outlet/>
       </Fragment>
     )
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app'
-import { getAuth, signInWithRedirect, signInWithPopup ,GoogleAuthProvider , createUserWithEmailAndPassword} from 'firebase/auth'
+import { getAuth, signInWithRedirect, signInWithPopup ,GoogleAuthProvider , createUserWithEmailAndPassword, signInWithEmailAndPassword} from 'firebase/auth'
 import{
   getFirestore,
   doc,
@@ -55,4 +55,9 @@ const firebaseConfig = {
     if(!email|| !password) return;
     return await createUserWithEmailAndPassword(auth,email,password);
   }
+
+  export const createSignInWithEmailAndPassword = async (email,password)=>{
+    if(!email|| !password) return;
+    return await signInWithEmailAndPassword(auth, email,password);
   
+  }
