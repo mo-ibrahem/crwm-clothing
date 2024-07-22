affca2c691f7b599dbb9c8a5c87e04a72e1e2f9c
Mohamed Ibrahim <57298210+mo-ibrahem@users.noreply.github.com>
2024-07-22 20:01:25 +0100

    thunk redux

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -4,17 +4,13 @@ import './shop.styles.scss'
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component'
 import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-import { setCategoriesMap } from '../../store/categories/category.action';
+import { fetchCategoriesAsync } from '../../store/categories/category.action';
 import { useDispatch } from 'react-redux';
 const Shop = () =>{
 
     const dispatch = useDispatch()
     useEffect(() =>{
-        const getCaregoriesMap = async () =>{
-            const categoriesArray = await getCategoriesAndDocuments()
-            dispatch(setCategoriesMap(categoriesArray))
-        }
-        getCaregoriesMap()
+      dispatch(fetchCategoriesAsync())
     },[])
 
     return(
--- src/store/store.js
+++ src/store/store.js
@@ -3,9 +3,9 @@ import logger from 'redux-logger'
 import { rootReducer } from "./root-reducer";
 import storage from 'redux-persist/lib/storage'
 import {persistStore , persistReducer} from "redux-persist";
+import {thunk} from 'redux-thunk'
 
-
-const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(Boolean)
+const middleWares = [process.env.NODE_ENV === 'development' && logger, thunk].filter(Boolean)
 const composeEnhancer = (process.env.NODEENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares))
 
